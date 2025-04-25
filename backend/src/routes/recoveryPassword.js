dd3654bfbe7fed51e75c8ff64c4ff4bc1ca4c670
Pablo Menjívar <134906528+pablo-menjivar@users.noreply.github.com>
2025-04-25 14:50:34 -0600

    route

--- backend/src/routes/recoveryPassword.js
+++ backend/src/routes/recoveryPassword.js
@@ -5,6 +5,7 @@ const router = express.Router()
 // Importo el controlador de recuperar contraseña
 import recoveryPasswordController from "../controllers/recoveryPasswordController.js"
 // Solo sería la ruta del Post o Create
-router.route("/").post(recoveryPasswordController.requestCode)
+router.route("/requestCode").post(recoveryPasswordController.requestCode)
+router.route("/verifyCode").post(recoveryPasswordController.verifyCode)
 // Exporto el router para poder usarlo en otros archivos
 export default router
