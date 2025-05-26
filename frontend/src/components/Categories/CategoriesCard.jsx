1e86b1efb6717d3ec6203f8f37a55797def8763e
Pablo Menjívar <134906528+pablo-menjivar@users.noreply.github.com>
2025-05-25 21:36:51 -0600

    CRUD de categorías terminado en la parte del Frontend

---
+++ frontend/src/components/Categories/CategoriesCard.jsx
@@ -0,0 +1,20 @@
+import React from "react"
+import Button from "../Button"
+const CategoryCard = ({ category, deleteCategory, updateCategories }) => {
+  return (
+    <div className="max-w-md mx-auto bg-white shadow-lg rounded-lg overflow-hidden border border-gray-200">
+      <div className="px-6 py-4">
+        <h2 className="text-xl font-bold text-gray-800 mb-2">
+          Categoría:{" "}
+          <span className="text-xl font-medium text-gray-700">
+            {category.name}{" "}
+          </span>
+        </h2>
+        <p className="text-gray-500 font-bold">{category.description}</p>
+        <Button label={"Eliminar"} actionButton={() => deleteCategory(category._id)} colorClass={"danger"}/>
+        <Button  label={"Editar Información"} actionButton={() => updateCategories(category)} colorClass={"warning"} />
+      </div>
+    </div>
+  )
+}
+export default CategoryCard
